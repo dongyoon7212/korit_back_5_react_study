c5d6bf9d87d77d726f43f1d9f7cc1e267b6e66e0
dongyoon7212 <dongyoon7212@naver.com>
2024-02-19 11:46:34 +0900

    feat: 배열 spread filter응용

--- function_study/spread.js
+++ function_study/spread.js
@@ -22,3 +22,29 @@ const nums2 = [...nums, 6, 7, 8, 9, 10];
 const nums3 = [...nums2.filter((n) => n % 2 === 0), 11, 12, 13, 14, 15];
 
 console.log(nums3);
+
+const users = [
+    {
+        id: 1,
+        name: "이동윤",
+    },
+    {
+        id: 2,
+        name: "삼동윤",
+    },
+    {
+        id: 3,
+        name: "사동윤",
+    },
+    {
+        id: 4,
+        name: "오동윤",
+    },
+];
+
+const evenUsers = [
+    ...users.filter((user) => user.id % 2 === 0),
+    { id: 5, name: "육동윤" },
+];
+
+console.log(evenUsers);
