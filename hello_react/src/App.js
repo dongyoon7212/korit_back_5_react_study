169102fd736e7086ee11c3f172402da62fbd45c2
dongyoon7212 <dongyoon7212@naver.com>
2024-02-19 17:31:07 +0900

    feat: react useState

--- hello_react/src/App.js
+++ hello_react/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * react component 특징
+ * 1. 파일명과 함수명을 일치시킨다.
+ * 2. 하나의 컴포넌트 함수는 하나의 태그 묶음만 리턴할 수 있다.
+ * 3. 함수를 꼭 export 해야한다.
+ *
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react";
+
+export default function App() {
+    //jsx 자료형
+    // let names = ["김준일", "김준이", "김준삼"];
+    // useState 비구조 할당
+    // const [num1, num2, num3, num4] = [1, 2, 3, 4];
+    const [nameArrayState, setNameArrayState] = useState([
+        "김준일",
+        "김준이",
+        "김준삼",
+    ]);
+
+    console.log("콘솔 호출?")
+
+    const handleClick = () => {
+        setNameArrayState([...nameArrayState, "김준사"]);
+    };
+    // 상태관리
+
+    return (
+        // 아무런 의미 없는 묶어주기 위한 태그 <Fragment></Fragment>
+        // jsx
+        <>
+            <button onClick={handleClick}>추가</button>
+            <div>
+                {nameArrayState.map((name) => (
+                    <h1>{name}</h1>
+                ))}
+            </div>
+        </>
+    );
 }
-
-export default App;
