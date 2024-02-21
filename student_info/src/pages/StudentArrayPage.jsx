1f6e5e89dca4d1b34a577df73d7c240a7be7160f
dongyoon7212 <dongyoon7212@naver.com>
2024-02-21 10:35:20 +0900

    feat: studentList map사용해서 출력

--- student_info/src/pages/StudentArrayPage.jsx
+++ student_info/src/pages/StudentArrayPage.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function StudentArrayPage(props) {
     const [studentList, setStudentList] = useState([]);
 
-    const [student, setStudent] = useState({
-        id: 0,
-        name: "",
-        age: 0,
-        address: "",
-    });
     const [inputValue, setInputValue] = useState({
         name: "",
         age: "",
         address: "",
     });
+
+    const staticId = useRef(0);
+    // staticId.current값이 변해도 렌더링 X
+    // 재렌더링이 발생해도 초기화되지 않음
+
+    useEffect(() => {
+        console.log(studentList);
+    }, [studentList]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
@@ -23,7 +25,14 @@ function StudentArrayPage(props) {
         });
     };
 
-    const handleAddClick = () => {};
+    const handleAddClick = () => {
+        const student = {
+            ...inputValue,
+            id: (staticId.current += 1),
+        };
+
+        setStudentList([...studentList, student]);
+    };
 
     return (
         <div>
@@ -51,6 +60,28 @@ function StudentArrayPage(props) {
                 />
                 <button onClick={handleAddClick}>추가</button>
             </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>이름</th>
+                        <th>나이</th>
+                        <th>주소</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {studentList.map((student) => {
+                        return (
+                            <tr key={student.id}>
+                                <td>{student.id}</td>
+                                <td>{student.name}</td>
+                                <td>{student.age}</td>
+                                <td>{student.address}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 }
