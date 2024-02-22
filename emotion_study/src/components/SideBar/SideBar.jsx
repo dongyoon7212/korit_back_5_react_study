9ef127b2db187f24fc97aabc40e075844113a6e0
dongyoon7212 <dongyoon7212@naver.com>
2024-02-22 16:50:29 +0900

    feat: SideBar menu, style

--- emotion_study/src/components/SideBar/SideBar.jsx
+++ emotion_study/src/components/SideBar/SideBar.jsx
@@ -1,14 +1,46 @@
 /** @jsxImportSource @emotion/react */
 
+import { useMemo, useState } from "react";
 import * as S from "./style";
-import { FaCaretRight } from "react-icons/fa";
+import { FaCaretRight, FaCaretLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function SideBar() {
+    const [isShow, setIsShow] = useState(false);
+
+    const menus = useMemo(
+        () => [
+            {
+                id: 1,
+                path: "/mymage",
+                name: "마이페이지",
+            },
+            {
+                id: 2,
+                path: "/board",
+                name: "게시판",
+            },
+            {
+                id: 3,
+                path: "/notice",
+                name: "공지사항",
+            },
+        ],
+        []
+    );
+
     return (
-        <aside css={S.layout}>
-            <button css={S.toggleButton}>
-                <FaCaretRight />
+        <aside css={S.layout(isShow)}>
+            <button css={S.toggleButton} onClick={() => setIsShow(!isShow)}>
+                {isShow ? <FaCaretLeft /> : <FaCaretRight />}
             </button>
+            <ul>
+                {menus.map((menu) => (
+                    <Link to={menu.path} key={menu.id}>
+                        <li>{menu.name}</li>
+                    </Link>
+                ))}
+            </ul>
         </aside>
     );
 }
--- emotion_study/src/components/SideBar/style.js
+++ emotion_study/src/components/SideBar/style.js
@@ -1,16 +1,42 @@
 import { css } from "@emotion/react";
 
-export const layout = css`
+export const layout = (isShow) => css`
+    box-sizing: border-box;
     position: fixed;
     top: 0;
-    left: 0;
+    left: ${isShow ? "0px" : "-300px"};
+    
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+    border-right: 1px solid black;
 
     width: 300px;
     height: 100%;
-
-    background-color: black;
+    transition: left 0.5s ease-in-out;
+    background-color: white;
 `;
 
 export const toggleButton = css`
-    
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    right: -15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    border: 1px solid #dbdbdb;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    width: 15px;
+    height: 50px;
+    cursor: pointer;
+    background-color: white;
+    &:hover {
+        background-color: #eee;
+    }
+    &:active {
+        background-color: #ccc;
+    }
 `;
