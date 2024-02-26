d95c4cc5eb1743beda8a3f04f9875b5970851aec
dongyoon7212 <dongyoon7212@naver.com>
2024-02-26 16:21:28 +0900

    feat: firebase upload, progressBar

--- emotion_study/src/pages/ImageEx/ImageEx.jsx
+++ emotion_study/src/pages/ImageEx/ImageEx.jsx
@@ -1,7 +1,10 @@
 /** @jsxImportSource @emotion/react */
 
 import { css } from "@emotion/react";
+import { ref, uploadBytesResumable } from "firebase/storage";
 import { useRef, useState } from "react";
+import { storage } from "../../configs/firebase/firebaseConfig";
+import { Line } from "rc-progress";
 
 const layout = css`
     display: flex;
@@ -25,13 +28,22 @@ const imageLayout = css`
 `;
 
 function ImageEx() {
+    const [uploadFiles, setUploadFiles] = useState([]);
     const [previews, setPreviews] = useState([]);
+    const [progressPercent, setProgressPercent] = useState(0);
 
     const imgFileRef = useRef();
 
     const handleImgFileChange = (e) => {
         //map함수를 사용하기 위해 일반 배열로 바꿔준다.
         const files = Array.from(e.target.files);
+
+        if (files.length === 0) {
+            imgFileRef.current.value = "";
+            return;
+        }
+
+        setUploadFiles(files);
 
         let promises = [];
 
@@ -71,12 +83,42 @@ function ImageEx() {
         });
     };
 
+    const handleImageUpload = () => {
+        const file = uploadFiles[0];
+        const storageRef = ref(storage, `files/test/${file.name}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                // 진행 중 상태
+                setProgressPercent(
+                    Math.round(
+                        snapshot.bytesTransferred / snapshot.totalBytes
+                    ) * 100
+                );
+            },
+            (error) => {}, // 에러 처리
+            () => {
+                // 완료된 후 처리
+                alert("업로드 완료")
+            }
+        );
+    };
+
     return (
         <div css={layout}>
             {previews.map((preview, index) => (
-                <div key={index} css={imageLayout}>
-                    <img src={preview} alt="" />
-                </div>
+                <>
+                    <div key={index} css={imageLayout}>
+                        <img src={preview} alt="" />
+                    </div>
+                    <Line
+                        percent={progressPercent}
+                        strokeWidth={4}
+                        strokeColor={"#22222"}
+                    />
+                </>
             ))}
             <input
                 style={{ display: "none" }}
@@ -88,6 +130,7 @@ function ImageEx() {
             <button onClick={() => imgFileRef.current.click()}>
                 이미지 불러오기
             </button>
+            <button onClick={handleImageUpload}>이미지 업로드</button>
         </div>
     );
 }
