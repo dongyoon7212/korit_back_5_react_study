759c13ad48a4500ebeefa87bff680a87b36c9e1b
dongyoon7212 <dongyoon7212@naver.com>
2024-02-28 16:24:44 +0900

    feat: boardList add style

--- emotion_study/src/pages/BoardList/BoardList.jsx
+++ emotion_study/src/pages/BoardList/BoardList.jsx
@@ -16,13 +16,49 @@ const headerTitle = css`
     font-weight: 700;
 `;
 
+const boardListLayout = css`
+    box-sizing: border-box;
+    border: 1px solid #dbdbdb;
+    width: 900px;
+    height: 500px;
+`;
+
+const boardListHeader = css`
+    box-sizing: border-box;
+    display: flex;
+    border-bottom: 1px solid #dbdbdb;
+    width: 100%;
+    & > div {
+        box-sizing: border-box;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-grow: 1;
+        height: 40px;
+        font-weight: 700;
+        cursor: default;
+    }
+    & > div:nth-of-type(1) {
+        flex-grow: 0;
+        border-right: 1px solid #dbdbdb;
+        width: 80px;
+    }
+`;
+
 function BoardList() {
     return (
         <div css={layout}>
             <h1 css={headerTitle}>게시글 목록</h1>
-            <ul>
+            <ul css={boardListLayout}>
+                <li css={boardListHeader}>
+                    <div>번호</div>
+                    <div>제목</div>
+                </li>
                 <Link to={"/board/1"}>
-                    <li></li>
+                    <li css={boardListHeader}>
+                        <div>1</div>
+                        <div>test1</div>
+                    </li>
                 </Link>
             </ul>
         </div>
