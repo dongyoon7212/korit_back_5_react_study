8abbad2ff778e5fe6128291712412b4b3c27a4ff
dongyoon7212 <dongyoon7212@naver.com>
2024-02-28 15:48:32 +0900

    feat: boardList

--- emotion_study/src/pages/BoardList/BoardList.jsx
+++ emotion_study/src/pages/BoardList/BoardList.jsx
@@ -1,14 +1,32 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { Link } from "react-router-dom";
 
 const layout = css`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 100px;
+`;
+
+const headerTitle = css`
+    margin-bottom: 30px;
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
 `;
 
 function BoardList() {
-    return <div></div>;
+    return (
+        <div css={layout}>
+            <h1 css={headerTitle}>게시글 목록</h1>
+            <ul>
+                <Link to={"/board/1"}>
+                    <li></li>
+                </Link>
+            </ul>
+        </div>
+    );
 }
 
 export default BoardList;
