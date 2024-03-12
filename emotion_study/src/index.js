eb4ca96552c0b9483d173923c0487647e0158515
dongyoon7212 <dongyoon7212@naver.com>
2024-03-12 15:28:06 +0900

    feat: emotion_study

--- emotion_study/src/index.js
+++ emotion_study/src/index.js
@@ -4,21 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import ComponentStudy from "./pages/ComponentStudy/ComponentStudy";
+// import ComponentStudy from "./pages/ComponentStudy/ComponentStudy";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const { a, b, c } = {
-    a: 10,
-    b: 20,
-    c: 30,
-};
+// const { a, b, c } = {
+//     a: 10,
+//     b: 20,
+//     c: 30,
+// };
 
 root.render(
-    // <BrowserRouter>
-    //     <App />
-    // </BrowserRouter>
-    <ComponentStudy a={a} b={b} c={c} />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    // <ComponentStudy a={a} b={b} c={c} />
 );
 
 reportWebVitals();
