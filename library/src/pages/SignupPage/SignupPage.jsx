56950c1e7875f8219ebce25f2f37304aab0a9e29
dongyoon7212 <dongyoon7212@naver.com>
2024-03-13 15:12:18 +0900

    feat: SignupPage

--- library/src/pages/SignupPage/SignupPage.jsx
+++ library/src/pages/SignupPage/SignupPage.jsx
@@ -10,33 +10,59 @@ function SignupPage() {
     const [checkPassword, setCheckPassword, checkPasswordChange] = useInput();
     const [name, setName, nameChange] = useInput();
     const [email, setEmail, emailChange] = useInput();
-    
+
+    const handleSignupSubmit = () => {
+        const signupData = {
+            username,
+            password,
+            checkPassword,
+            name,
+            email,
+        };
+    };
+
     return (
         <>
             <div css={s.header}>
                 <h1>회원가입</h1>
-                <RightTopButton>가입하기</RightTopButton>
+                <RightTopButton onClick={handleSignupSubmit}>
+                    가입하기
+                </RightTopButton>
             </div>
             <AuthPageInput
                 type={"text"}
                 name={"username"}
                 placeholder={"사용자이름"}
+                value={username}
+                onChange={usernameChange}
             />
             <AuthPageInput
                 type={"password"}
                 name={"password"}
                 placeholder={"비밀번호"}
+                value={password}
+                onChange={passwordChange}
             />
             <AuthPageInput
                 type={"password"}
                 name={"checkPassword"}
                 placeholder={"비밀번호 확인"}
+                value={checkPassword}
+                onChange={checkPasswordChange}
             />
-            <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} />
+            <AuthPageInput
+                type={"text"}
+                name={"name"}
+                placeholder={"성명"}
+                value={name}
+                onChange={nameChange}
+            />
             <AuthPageInput
                 type={"text"}
                 name={"email"}
                 placeholder={"이메일"}
+                value={email}
+                onChange={emailChange}
             />
         </>
     );
