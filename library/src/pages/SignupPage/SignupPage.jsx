7ea4e6bae0ca3d7c94e3764483f918ea9bacbfdc
dongyoon7212 <dongyoon7212@naver.com>
2024-03-14 10:46:33 +0900

    feat: valid check

--- library/src/pages/SignupPage/SignupPage.jsx
+++ library/src/pages/SignupPage/SignupPage.jsx
@@ -4,6 +4,7 @@ import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
 import RightTopButton from "../../components/RightTopButton/RightTopButton";
 import { useInput } from "../../hooks/useInput";
 import axios from "axios";
+import { useState } from "react";
 
 function SignupPage() {
     const [username, setUsername, usernameChange] = useInput();
@@ -11,6 +12,14 @@ function SignupPage() {
     const [checkPassword, setCheckPassword, checkPasswordChange] = useInput();
     const [name, setName, nameChange] = useInput();
     const [email, setEmail, emailChange] = useInput();
+
+    const [messageGroup, setMessageGroup] = useState({
+        username: null,
+        password: null,
+        checkPassword: null,
+        name: null,
+        email: null,
+    });
 
     const handleSignupSubmit = () => {
         const signupData = {
@@ -32,7 +41,41 @@ function SignupPage() {
             );
             console.log(response);
         } catch (error) {
-            console.log(error);
+            // 200 300이 아니면 오류를 axios에서 catch로 넘겨버림
+            const errorMap = error.response.data;
+            const entries = Object.entries(errorMap);
+            let newMessageGroup = {
+                username: {
+                    type: "success",
+                    text: "사용할 수 있는 사용자이름 입니다.",
+                },
+                password: {
+                    type: "success",
+                    text: "",
+                },
+                checkPassword: {
+                    type: "success",
+                    text: "",
+                },
+                name: {
+                    type: "success",
+                    text: "",
+                },
+                email: {
+                    type: "success",
+                    text: "사용할 수 있는 이메일 입니다.",
+                },
+            };
+            for (let [key, value] of entries) {
+                newMessageGroup = {
+                    ...newMessageGroup,
+                    [key]: {
+                        type: "error",
+                        text: value,
+                    },
+                };
+            }
+            setMessageGroup(() => newMessageGroup);
         }
     };
 
@@ -50,6 +93,7 @@ function SignupPage() {
                 placeholder={"사용자이름"}
                 value={username}
                 onChange={usernameChange}
+                message={messageGroup.username}
             />
             <AuthPageInput
                 type={"password"}
@@ -57,6 +101,7 @@ function SignupPage() {
                 placeholder={"비밀번호"}
                 value={password}
                 onChange={passwordChange}
+                message={messageGroup.password}
             />
             <AuthPageInput
                 type={"password"}
@@ -64,6 +109,7 @@ function SignupPage() {
                 placeholder={"비밀번호 확인"}
                 value={checkPassword}
                 onChange={checkPasswordChange}
+                message={messageGroup.checkPassword}
             />
             <AuthPageInput
                 type={"text"}
@@ -71,6 +117,7 @@ function SignupPage() {
                 placeholder={"성명"}
                 value={name}
                 onChange={nameChange}
+                message={messageGroup.name}
             />
             <AuthPageInput
                 type={"text"}
@@ -78,6 +125,7 @@ function SignupPage() {
                 placeholder={"이메일"}
                 value={email}
                 onChange={emailChange}
+                message={messageGroup.email}
             />
         </>
     );
