1d38b7809392a4dda1552ff2f0bee6fa798927d8
dongyoon7212 <dongyoon7212@naver.com>
2024-03-15 10:11:32 +0900

    feat: javascript 정규식

--- library/src/pages/SignupPage/SignupPage.jsx
+++ library/src/pages/SignupPage/SignupPage.jsx
@@ -3,138 +3,17 @@ import * as s from "./style";
 import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
 import RightTopButton from "../../components/RightTopButton/RightTopButton";
 import { useInput } from "../../hooks/useInput";
-import axios from "axios";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SignupPage() {
     const navigate = useNavigate();
-    const [username, setUsername, usernameChange] = useInput();
-    const [password, setPassword, passwordChange] = useInput();
-    const [checkPassword, setCheckPassword, checkPasswordChange] = useInput();
-    const [name, setName, nameChange] = useInput();
-    const [email, setEmail, emailChange] = useInput();
+    const [username, usernameChange, usernameMessage] = useInput("username");
+    const [password, passwordChange, passwordMessage] = useInput("password");
+    const [checkPassword, checkPasswordChange] = useInput("checkPassword");
+    const [name, nameChange, nameMessage] = useInput("name");
+    const [email, emailChange, emailMessage] = useInput("email");
 
-    const [messageGroup, setMessageGroup] = useState({
-        username: null,
-        password: null,
-        checkPassword: null,
-        name: null,
-        email: null,
-    });
-
-    const handleCheckPassword = (e) => {
-        if (!!e.target.value) {
-            setMessageGroup((messageGroup) => {
-                return {
-                    ...messageGroup,
-                    checkPassword: {
-                        type: checkPassword === password ? "success" : "error",
-                        text:
-                            checkPassword === password
-                                ? ""
-                                : "비밀번호가 서로 일치하지 않습니다",
-                    },
-                };
-            });
-        } else {
-            setMessageGroup((messageGroup) => {
-                return {
-                    ...messageGroup,
-                    checkPassword: null,
-                };
-            });
-        }
-    };
-
-    const handleSignupSubmit = () => {
-        if (messageGroup?.checkPassword?.type === "error") {
-            alert("비밀번호가 서로 일치하지 않습니다.");
-            return;
-        }
-
-        if (!checkPassword) {
-            setMessageGroup((messageGroup) => {
-                return {
-                    ...messageGroup,
-                    checkPassword: {
-                        type: "error",
-                        text: "비밀번호를 입력하세요",
-                    },
-                };
-            });
-            return;
-        }
-        const signupData = {
-            username,
-            password,
-            checkPassword,
-            name,
-            email,
-        };
-
-        signupRequest(signupData);
-    };
-
-    const signupRequest = async (signupData) => {
-        try {
-            const response = await axios.post(
-                "http://localhost:8080/auth/signup",
-                signupData
-            );
-            if (response.data) {
-                alert("회원가입이 완료되었습니다.");
-                navigate("/auth/signin");
-            }
-            console.log(response.data);
-            //정상적으로 등록이 되면 true를 반환
-        } catch (error) {
-            // 200 300이 아니면 오류를 axios에서 catch로 넘겨버림
-            const errorMap = error.response.data;
-            const entries = Object.entries(errorMap);
-            let newMessageGroup = {
-                username: {
-                    type: "success",
-                    text: "사용할 수 있는 사용자이름 입니다.",
-                },
-                password: {
-                    type: "success",
-                    text: "",
-                },
-                checkPassword: {
-                    type: "success",
-                    text: "",
-                },
-                name: {
-                    type: "success",
-                    text: "",
-                },
-                email: {
-                    type: "success",
-                    text: "사용할 수 있는 이메일 입니다.",
-                },
-            };
-            for (let [key, value] of entries) {
-                newMessageGroup = {
-                    ...newMessageGroup,
-                    [key]: {
-                        type: "error",
-                        text: value,
-                    },
-                };
-            }
-            if (newMessageGroup.password.type === "error") {
-                newMessageGroup = {
-                    ...newMessageGroup,
-                    checkPassword: null,
-                };
-                setPassword(() => "");
-                setCheckPassword(() => "");
-            }
-            setMessageGroup(() => newMessageGroup);
-        }
-    };
-
+    const handleSignupSubmit = () => {};
     return (
         <>
             <div css={s.header}>
@@ -149,7 +28,7 @@ function SignupPage() {
                 placeholder={"사용자이름"}
                 value={username}
                 onChange={usernameChange}
-                message={messageGroup.username}
+                message={usernameMessage}
             />
             <AuthPageInput
                 type={"password"}
@@ -157,7 +36,7 @@ function SignupPage() {
                 placeholder={"비밀번호"}
                 value={password}
                 onChange={passwordChange}
-                message={messageGroup.password}
+                message={passwordMessage}
             />
             <AuthPageInput
                 type={"password"}
@@ -165,8 +44,7 @@ function SignupPage() {
                 placeholder={"비밀번호 확인"}
                 value={checkPassword}
                 onChange={checkPasswordChange}
-                onBlur={handleCheckPassword}
-                message={messageGroup.checkPassword}
+                message={null}
             />
             <AuthPageInput
                 type={"text"}
@@ -174,7 +52,7 @@ function SignupPage() {
                 placeholder={"성명"}
                 value={name}
                 onChange={nameChange}
-                message={messageGroup.name}
+                message={nameMessage}
             />
             <AuthPageInput
                 type={"text"}
@@ -182,7 +60,7 @@ function SignupPage() {
                 placeholder={"이메일"}
                 value={email}
                 onChange={emailChange}
-                message={messageGroup.email}
+                message={emailMessage}
             />
         </>
     );
