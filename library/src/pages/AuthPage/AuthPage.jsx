b843d81669d044276817722fd596c393cf5b633a
dongyoon7212 <dongyoon7212@naver.com>
2024-03-15 12:17:39 +0900

    feat: Login form

--- library/src/pages/AuthPage/AuthPage.jsx
+++ library/src/pages/AuthPage/AuthPage.jsx
@@ -2,12 +2,13 @@
 import { Route, Routes } from "react-router-dom";
 import * as s from "./style";
 import SignupPage from "../SignupPage/SignupPage";
+import Signin from "../SigninPage/Signin";
 
 function AuthPage() {
     return (
         <div css={s.layout}>
             <Routes>
-                <Route path="/signin" />
+                <Route path="/signin" element={<Signin />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/signup/oauth" />
             </Routes>
