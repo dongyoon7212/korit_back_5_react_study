191694f8fe990658ef6a2351e0268af19b46bc13
dongyoon7212 <dongyoon7212@naver.com>
2024-03-19 17:09:07 +0900

    feat: MyPage

--- library/src/pages/MyPage/MyPage.jsx
+++ library/src/pages/MyPage/MyPage.jsx
@@ -1,10 +1,32 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
+import img from "../../assets/bio-photo.jpg";
+import { useQueryClient } from "react-query";
 
 function MyPage() {
+    const queryClient = useQueryClient();
+    const principalData = queryClient.getQueryData("principalQuery");
     return (
         <div css={s.layout}>
-            <div css={s.header}></div>
+            <div css={s.header}>
+                <div css={s.imgBox}>
+                    <div css={s.profileImg}>
+                        <img src={img} alt="" />
+                    </div>
+                </div>
+                <div css={s.infoBox}>
+                    <div css={s.infoText}>
+                        사용자 이름 : {principalData.data.username}
+                    </div>
+                    <div css={s.infoText}>이름 : {principalData.data.name}</div>
+                    <div>
+                        <div css={s.infoText}>이메일 : {principalData.data.email}</div>
+                        <button>인증하기</button>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <div css={s.bottom}></div>
         </div>
     );
 }
--- library/src/pages/MyPage/style.js
+++ library/src/pages/MyPage/style.js
@@ -9,9 +9,53 @@ export const layout = css`
 
 export const header = css`
     box-sizing: border-box;
+    display: flex;
+    margin-bottom: 20px;
     border: 1px solid #dbdbdb;
     border-radius: 3px;
+    padding: 10px;
     width: 100%;
     height: 200px;
+    background-color: white;
+`;
+
+export const imgBox = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 178px;
+    height: 100%;
+`;
+
+export const profileImg = css`
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid #dbdbdb;
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+
+    & > img {
+        height: 100%;
+    }
+`;
+
+export const infoBox = css``;
+
+export const infoText = css``;
+
+export const bottom = css`
+    box-sizing: border-box;
+    display: flex;
+    margin-bottom: 20px;
+    border: 1px solid #dbdbdb;
+    border-radius: 3px;
+    padding: 10px;
+    width: 100%;
+    height: 250px;
     background-color: white;
 `;
