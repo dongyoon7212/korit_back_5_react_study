ee202c2584645d820c99255bae921d9f553343c9
dongyoon7212 <dongyoon7212@naver.com>
2024-03-19 09:48:21 +0900

    feat: react-query settings

--- library/src/Routes/AuthRoute.jsx
+++ library/src/Routes/AuthRoute.jsx
@@ -1,27 +1,29 @@
 import { Route, Routes } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { principalState } from "../atoms/principalAtom";
-import { useCallback, useEffect } from "react";
-import { getPricipalRequest } from "../apis/api/principal";
 import AuthPage from "../pages/AuthPage/AuthPage";
 import HomePage from "../pages/HomePage/HomePage";
+import { useQuery } from "react-query";
+import { getPricipalRequest } from "../apis/api/principal";
+
+// useQuery => GET 요청시에 사용
+// 첫번째 매개변수 => 배열 ["key값", dependency]
+// 두번째 매개변수 => 요청메소드(async, await)
+// 세번째 매개변수 => 옵션
+//  {
+//      retry: 0,
+//      refetchOnWindowFocus: false, 화면이 포커스 될때마다 재요청
+//      onSuccess: 함수,
+//      onError: 함수,
+//      enabled: true or false
+//  }
 
 function AuthRoute(props) {
-    const [principal, setPrincipal] = useRecoilState(principalState);
-
-    useEffect(() => {
-        getPrincipal();
-    }, []);
-
-    const getPrincipal = useCallback(() => {
-        getPricipalRequest()
-            .then((response) => {
-                setPrincipal(() => response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+    const getPrincipal = useQuery(["getPrincipal"], getPricipalRequest, {
+        refetchOnWindowFocus: false,
+        onSuccess: (response) => {
+            console.log("getPrincipal 요청");
+            console.log(response);
+        },
+    });
 
     return (
         <Routes>
