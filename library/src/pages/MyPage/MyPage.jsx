8070a5550f88deea008c993eb23f299cb810705e
dongyoon7212 <dongyoon7212@naver.com>
2024-03-20 12:50:43 +0900

    feat: 이메일 인증

--- library/src/pages/MyPage/MyPage.jsx
+++ library/src/pages/MyPage/MyPage.jsx
@@ -1,44 +1,70 @@
 /** @jsxImportSource @emotion/react */
 import * as s from "./style";
 import img from "../../assets/bio-photo.jpg";
-import { useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
+import { sendAuthMailRequest } from "../../apis/api/sendAuthMail";
+import FullSizeLoader from "../../components/FullSizeLoader/FullSizeLoader";
 
 function MyPage() {
     const queryClient = useQueryClient();
     const principalData = queryClient.getQueryData("principalQuery");
+
+    const sendAuthMailMutation = useMutation({
+        mutationKey: "sendAuthMailMutation",
+        mutationFn: sendAuthMailRequest,
+        retry: 0,
+    });
+
+    const handleSendAuthMailClick = () => {
+        sendAuthMailMutation.mutate();
+    };
+
     return (
-        <div css={s.layout}>
-            <div css={s.header}>
-                <div css={s.imgBox}>
-                    <div css={s.profileImg}>
-                        <img src={img} alt="" />
-                    </div>
-                </div>
-                <div css={s.infoBox}>
-                    <div css={s.infoText}>
-                        사용자 이름 : {principalData.data.username}
-                    </div>
-                    <div css={s.infoText}>이름 : {principalData.data.name}</div>
-                    <div css={s.emailBox}>
-                        <div css={s.infoText}>
-                            이메일 : {principalData.data.email}
+        <>
+            {sendAuthMailMutation.isLoading ? (
+                <FullSizeLoader />
+            ) : (
+                <div css={s.layout}>
+                    <div css={s.header}>
+                        <div css={s.imgBox}>
+                            <div css={s.profileImg}>
+                                <img src={img} alt="" />
+                            </div>
                         </div>
-                        {principalData.data.authorities.filter(
-                            (auth) => auth.authority === "ROLE_USER"
-                        ).length === 0 ? (
-                            <button css={s.infoButton}>인증하기</button>
-                        ) : (
-                            <>체크</>
-                        )}
+                        <div css={s.infoBox}>
+                            <div css={s.infoText}>
+                                사용자 이름 : {principalData.data.username}
+                            </div>
+                            <div css={s.infoText}>
+                                이름 : {principalData.data.name}
+                            </div>
+                            <div css={s.emailBox}>
+                                <div css={s.infoText}>
+                                    이메일 : {principalData.data.email}
+                                </div>
+                                {principalData.data.authorities.filter(
+                                    (auth) => auth.authority === "ROLE_USER"
+                                ).length === 0 ? (
+                                    <button
+                                        css={s.infoButton}
+                                        onClick={handleSendAuthMailClick}
+                                    >
+                                        인증하기
+                                    </button>
+                                ) : (
+                                    <>체크</>
+                                )}
+                            </div>
+                            <div>
+                                <button>정보 수정</button>
+                                <button>비밀번호 수정</button>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        <button>정보 수정</button>
-                        <button>비밀번호 수정</button>
-                    </div>
+                    <div css={s.bottom}></div>
                 </div>
-            </div>
-            <div css={s.bottom}></div>
-        </div>
+            )}
+        </>
     );
 }
 
