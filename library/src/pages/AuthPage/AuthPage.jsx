51a5602fcd791e6e894b93db25c11074989307b4
dongyoon7212 <dongyoon7212@naver.com>
2024-03-21 17:18:28 +0900

    feat: oauth2 Signin

--- library/src/pages/AuthPage/AuthPage.jsx
+++ library/src/pages/AuthPage/AuthPage.jsx
@@ -5,6 +5,7 @@ import SignupPage from "../SignupPage/SignupPage";
 import Signin from "../SigninPage/Signin";
 import OAuth2Page from "../OAuth2Page/OAuth2Page";
 import OAuth2SignupPage from "../OAuth2SignupPage/OAuth2SignupPage";
+import OAuth2SigninPage from "../OAuth2SigninPage/OAuth2SigninPage";
 
 function AuthPage() {
     return (
@@ -13,6 +14,7 @@ function AuthPage() {
                 <Route path="/signin" element={<Signin />} />
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/oauth2" element={<OAuth2Page />} />
+                <Route path="/oauth2/signin" element={<OAuth2SigninPage />} />
                 <Route path="/oauth2/merge" />
                 <Route path="/oauth2/signup" element={<OAuth2SignupPage />} />
             </Routes>
