2308ae1b523b5317d6a20069da44e8f5edc59556
dongyoon7212 <dongyoon7212@naver.com>
2024-03-22 09:50:49 +0900

    feat: OAuth2MergePage

--- library/src/pages/OAuth2MergePage/OAuth2MergePage.jsx
+++ library/src/pages/OAuth2MergePage/OAuth2MergePage.jsx
@@ -1,19 +1,42 @@
 /** @jsxImportSource @emotion/react */
+import { useMutation } from "react-query";
 import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
 import RightTopButton from "../../components/RightTopButton/RightTopButton";
 import { useInput } from "../../hooks/useInput";
 import * as s from "./style";
+import { oAuth2MergeRequest } from "../../apis/api/oAuth2Merge";
+import { useSearchParams } from "react-router-dom";
 
 function OAuth2MergePage() {
     const [username, usernameChange] = useInput();
     const [password, passwordChange] = useInput();
+    const [searchParams] = useSearchParams();
 
-    const handleSigninSubmit = () => {};
+    const oAuth2MergeMutation = useMutation({
+        mutationKey: "oAuth2MergeMutation",
+        mutationFn: oAuth2MergeRequest,
+        onSuccess: (response) => {
+            alert("계정 통합이 완료되었습니다. \n 다시 로그인 하세요.");
+            window.location.replace("/auth/signin");
+        },
+        onError: (error) => {
+            alert(error.response.data);
+        },
+    });
+
+    const handleSigninSubmit = () => {
+        oAuth2MergeMutation.mutate({
+            username,
+            password,
+            oauth2Name: searchParams.get("name"),
+            providerName: searchParams.get("provider"),
+        });
+    };
 
     return (
         <>
             <div css={s.header}>
-                <h1>계정 통합 로그인</h1>
+                <h2>계정 통합 로그인</h2>
                 <RightTopButton onClick={handleSigninSubmit}>
                     로그인하기
                 </RightTopButton>
