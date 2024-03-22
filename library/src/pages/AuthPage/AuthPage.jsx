fa9494157ca2117edb4d87347e882ad4e197566a
dongyoon7212 <dongyoon7212@naver.com>
2024-03-22 09:21:31 +0900

    feat: OAuth2MergePage

--- library/src/pages/AuthPage/AuthPage.jsx
+++ library/src/pages/AuthPage/AuthPage.jsx
@@ -8,6 +8,7 @@ import OAuth2SignupPage from "../OAuth2SignupPage/OAuth2SignupPage";
 import OAuth2SigninPage from "../OAuth2SigninPage/OAuth2SigninPage";
 import { useEffect } from "react";
 import { useQueryClient } from "react-query";
+import OAuth2MergePage from "../OAuth2MergePage/OAuth2MergePage";
 
 function AuthPage() {
     const queryClient = useQueryClient();
@@ -27,7 +28,7 @@ function AuthPage() {
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/oauth2" element={<OAuth2Page />} />
                 <Route path="/oauth2/signin" element={<OAuth2SigninPage />} />
-                <Route path="/oauth2/merge" />
+                <Route path="/oauth2/merge" element={<OAuth2MergePage />} />
                 <Route path="/oauth2/signup" element={<OAuth2SignupPage />} />
             </Routes>
         </div>
--- library/src/pages/OAuth2MergePage/OAuth2MergePage.jsx
+++ library/src/pages/OAuth2MergePage/OAuth2MergePage.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
+import RightTopButton from "../../components/RightTopButton/RightTopButton";
+import { useInput } from "../../hooks/useInput";
+import * as s from "./style";
 
-function OAuth2MergePage(props) {
+function OAuth2MergePage() {
+    const [username, usernameChange] = useInput();
+    const [password, passwordChange] = useInput();
+
+    const handleSigninSubmit = () => {};
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div css={s.header}>
+                <h1>계정 통합 로그인</h1>
+                <RightTopButton onClick={handleSigninSubmit}>
+                    로그인하기
+                </RightTopButton>
+            </div>
+            <AuthPageInput
+                type={"text"}
+                name={"username"}
+                placeholder={"사용자이름"}
+                value={username}
+                onChange={usernameChange}
+            />
+            <AuthPageInput
+                type={"password"}
+                name={"password"}
+                placeholder={"비밀번호"}
+                value={password}
+                onChange={passwordChange}
+            />
+        </>
     );
 }
 
